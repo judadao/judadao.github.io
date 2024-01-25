468d5708d4b38ef7275ce4395ff27b5ea25cd067
harryTT.Ju <harryTT_Ju@moxa.com>
2024-01-25 10:23:00 +0800

    change content

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -64,20 +64,15 @@ const CONFIG = {
       '', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
     'Git',
     'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'CICD',
+    'RTOS',
+    'C/C++',
+    'JavaScript',
+    'Node.js',
+    'Pytorch'
+    
   ],
   experiences: [
     {
