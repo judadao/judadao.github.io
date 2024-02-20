f6ae9eac4145cd905d21a5882180e09b89805da9
judadao <harryju3@gmail.com>
2024-02-20 13:50:32 +0800

    linked fix

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -115,7 +115,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'nord',
+    defaultTheme: 'dim',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
